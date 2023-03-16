6177139c447926933778a6e0328b5370059decc8
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-03-16 15:21:45 +0200

    making changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+/* Mobile Menu */
+
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 
@@ -5,87 +7,12 @@ menuBtn.addEventListener('click', function () {
   menuBtn.classList.toggle('active');
   menu.classList.toggle('active');
 })
+function closeHamburger() {
+  let checkItem = document.getElementById('menu');
+  checkItem.checked = false;
+}  
 
-
-/*
-const sliders = [...document.querySelectorAll(".slider__container")];
-const sliderControlPrev = [...document.querySelectorAll(".slider__control.prev")];
-const sliderControlNext = [...document.querySelectorAll(".slider__control.next")];
-
-sliders.forEach((slider, i) => {
-  let isDragStart = false,
-      isDragging = false,
-      isSlide = false,
-      prevPageX,
-      prevScrollLeft,
-      positionDiff;
-
-  const sliderItem = slider.querySelector(".slider__item");
-  var isMultislide = (slider.dataset.multislide === 'true');
-
-  sliderControlPrev[i].addEventListener('click', () => {
-    if (isSlide) return;
-    isSlide = true;
-    let slideWidth = isMultislide ? slider.clientWidth : sliderItem.clientWidth;
-    slider.scrollLeft += -slideWidth;
-    setTimeout(function(){ isSlide = false; }, 700);
-  });
-
-  sliderControlNext[i].addEventListener('click', () => {
-    if (isSlide) return;
-    isSlide = true;
-    let slideWidth = isMultislide ? slider.clientWidth : sliderItem.clientWidth ;
-    slider.scrollLeft += slideWidth;
-    setTimeout(function(){ isSlide = false; }, 700);
-  });
-
-  function autoSlide() {
-    if(slider.scrollLeft - (slider.scrollWidth - slider.clientWidth) > -1 || slider.scrollLeft <= 0) return;
-    positionDiff = Math.abs(positionDiff);
-    let slideWidth = isMultislide ? slider.clientWidth : sliderItem.clientWidth;
-    let valDifference = slideWidth - positionDiff;
-    if(slider.scrollLeft > prevScrollLeft) {
-        return slider.scrollLeft += positionDiff > slideWidth / 5 ? valDifference : -positionDiff;
-    }
-    slider.scrollLeft -= positionDiff > slideWidth / 5 ? valDifference : -positionDiff;
-  }
-
-  function dragStart(e) {
-    if (isSlide) return;
-    isSlide = true;
-    isDragStart = true;
-    prevPageX = e.pageX || e.touches[0].pageX;
-    prevScrollLeft = slider.scrollLeft;
-    setTimeout(function(){ isSlide = false; }, 700);
-  }
-
-  function dragging(e) {
-    if(!isDragStart) return;
-    e.preventDefault();
-    isDragging = true;
-    slider.classList.add("dragging");
-    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-    slider.scrollLeft = prevScrollLeft - positionDiff;
-  }
-
-  function dragStop() {
-    isDragStart = false;
-    slider.classList.remove("dragging");
-    if(!isDragging) return;
-    isDragging = false;
-    autoSlide();
-  }
-
-  addEventListener("resize", autoSlide);
-  slider.addEventListener("mousedown", dragStart);
-  slider.addEventListener("touchstart", dragStart);
-  slider.addEventListener("mousemove", dragging);
-  slider.addEventListener("touchmove", dragging);
-  slider.addEventListener("mouseup", dragStop);
-  slider.addEventListener("touchend", dragStop);
-  slider.addEventListener("mouseleave", dragStop);
-});*/
-
+/* Slider */
 
 let swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
@@ -106,7 +33,7 @@ let swiper = new Swiper(".mySwiper", {
 swiper.slideTo(4, 0);
 
 
-
+/* Modal Window */
 const cards = document.querySelectorAll('.services__container_card');
 const modal = document.querySelector('.modal');
 const modalContent = document.querySelector('.modal-content');
@@ -116,20 +43,39 @@ cards.forEach(services__container_card => {
   services__container_card.addEventListener('click', () => {
     const service = services__container_card.dataset.service;
     const serviceDescription = services__container_card.querySelector('p').textContent;
-    
-    modalContent.querySelector('h3').textContent = service;
+
+    modalContent.querySelector('h4').textContent = service;
     modalContent.querySelector('p').textContent = serviceDescription;
 
-modal.style.display = 'block';
-});
+    modal.style.display = 'block';
+  });
 });
 
 closeBtn.addEventListener('click', () => {
-modal.style.display = 'none';
+  modal.style.display = 'none';
 });
 
 window.addEventListener('click', (event) => {
-if (event.target == modal) {
-modal.style.display = 'none';
-}
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
 });
+
+
+/* Scroll button 'Мої послуги' */
+
+function scrollToSection() {
+  let section = document.getElementById("services");
+  section.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+}
+
+/* Scroll button 'Забронювати' */
+
+function scrollToSection() {
+  let section = document.getElementById("services");
+  section.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+}
+
+
+
+
