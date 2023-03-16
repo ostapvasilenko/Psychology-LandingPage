a094a53b068208d694c7cd92464aa2269fcb77de
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-03-16 12:34:40 +0200

    make model window in Services

--- src/js/script.js
+++ src/js/script.js
@@ -104,3 +104,32 @@ let swiper = new Swiper(".mySwiper", {
   },
 });
 swiper.slideTo(4, 0);
+
+
+
+const cards = document.querySelectorAll('.services__container_card');
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
+const closeBtn = document.querySelector('.close-btn');
+
+cards.forEach(services__container_card => {
+  services__container_card.addEventListener('click', () => {
+    const service = services__container_card.dataset.service;
+    const serviceDescription = services__container_card.querySelector('p').textContent;
+    
+    modalContent.querySelector('h3').textContent = service;
+    modalContent.querySelector('p').textContent = serviceDescription;
+
+modal.style.display = 'block';
+});
+});
+
+closeBtn.addEventListener('click', () => {
+modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+if (event.target == modal) {
+modal.style.display = 'none';
+}
+});
